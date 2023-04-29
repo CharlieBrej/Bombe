7bca4f2340dad8396a6684016a3944777030f46a
brejc8 <github@brej.org>
2023-04-29 22:56:36 +0100

    Correct a selection

--- GameState.cpp
+++ GameState.cpp
@@ -2991,6 +2991,8 @@ void GameState::render(bool saving)
             r_type.type = menu_region_types[pos.y][pos.x];
             if (r_type.type == RegionType::NONE)
             {
+                if (game_mode == 3)
+                    continue;
                 if (pos.y)
                     continue;
                 r_type = RegionType(RegionType::EQUAL, 0);
@@ -4035,7 +4037,7 @@ void GameState::right_panel_click(XYPos pos, int clicks, int btn)
         {
             inspected_rule.rule->deleted = true;
             right_panel_mode = RIGHT_MENU_NONE;
-            if (inspected_rule.rule->apply_region_type.type != RegionType::VISIBILITY)
+            if (inspected_rule.rule->apply_region_type.type != RegionType::VISIBILITY && (game_mode == 2 || game_mode == 3))
                 reset_levels();
         }
         if ((pos - XYPos(button_size * 3, button_size * 2)).inside(XYPos(button_size * 2, button_size)))
@@ -4154,9 +4156,12 @@ void GameState::right_panel_click(XYPos pos, int clicks, int btn)
             RegionType::Type t = menu_region_types[region_item_selected.y][region_item_selected.x];
             if (t == RegionType::NONE)
             {
-                select_region_type.var ^= 1;
-                region_type = select_region_type;
+                if (game_mode != 3)
+                {
+                    select_region_type.var ^= 1;
+                    region_type = select_region_type;
 //                region_type.var ^= (1 << region_item_selected.y);
+                }
             }
             else
             {
@@ -4600,6 +4605,7 @@ bool GameState::events()
                             current_level_index = 0;
                             load_level = 1;
                             region_type = RegionType(RegionType::SET, 0);
+                            select_region_type = RegionType(RegionType::EQUAL, 0);
                             game_mode = index;
                         }
                         display_modes = false;
